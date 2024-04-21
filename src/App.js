c0f52995db1f45ca945a80eac7dd4b8760ab58b2
Omkar j <107001176+omkarjninja@users.noreply.github.com>
2024-04-22 02:57:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ function App() {
 
       <Routes>
         <Route exact path="/" element={<Home></Home>} />
-        <Route path="https://endearing-dragon-61ba6d.netlify.app/register" element={<Register></Register>} />
-        <Route path="https://endearing-dragon-61ba6d.netlify.app/template" element={<Template></Template>} />
+        <Route path="/register" element={<Register></Register>} />
+        <Route path="/template" element={<Template></Template>} />
         <Route path="/login" element={<Login></Login>} />
-        <Route path="https://endearing-dragon-61ba6d.netlify.app/resume" element={<Resume></Resume>} />
+        <Route path="/resume" element={<Resume></Resume>} />
         <Route path="/resume-temp" element={<Resume_Temp></Resume_Temp>} />
         <Route path="/resume-temp2" element={<Resume_Temp2></Resume_Temp2>} />
       </Routes>
